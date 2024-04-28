a84b6633e5a411a45415dfaf1c9da8957c99dec2
Chun Wu <the.chun.wu@gmail.com>
2024-04-28 00:34:40 -0700

    Implemented error handling pages for error 404 and 500.

--- app/routes.py
+++ app/routes.py
@@ -21,3 +21,19 @@ def login():
         )
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+# User Profile Page
+@app.route("/user/<name>")
+def user(name): 
+    return render_template('user.html', user_name=name)
+
+# Custom Error Page - Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Custom Error Page - Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
