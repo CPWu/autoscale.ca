21d3483f56a2b55a3211fc8ac9c94d583557ec5b
Chun Wu <the.chun.wu@gmail.com>
2024-05-09 18:30:09 -0700

    Implemented werkzeug.security for Password Hashing

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,6 +12,18 @@ class User(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     favourite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute!')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Create A String
     def __repr__(self):
